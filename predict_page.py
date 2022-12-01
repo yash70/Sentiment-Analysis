236303226962319070383f78ffa5f481e163fb32
Yash Taneja <62446644+yash70@users.noreply.github.com>
2022-12-01 16:25:40 +0530

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -9,11 +9,17 @@ tf = TfidfVectorizer()
 df = pd.read_csv('IMDB Dataset.csv')
 X = df['review']
 new_data = tf.fit_transform(X)
+percent_pos = pd.read_csv('percent_pos.csv')
+movies = percent_pos['Movie Title'].tolist()
+percent_pos.set_index('Movie Title', inplace=True)
+
 
 def load_model():
     with open('final_model.pkl', 'rb') as file:
         model = joblib.load(file)
     return model
+
+
 
 
 model = load_model()
@@ -23,6 +29,15 @@ def show_predict_page():
     st.title("Sentiment Analysis of Movie Reviews")
 
     name = st.text_input('Hey, what should we call you?')
+
+    st.write("## Enter Movie Name")
+    movie_name = st.selectbox('', movies)
+
+    if movie_name == 'Make a Selection':
+        st.write('You haven\'t made a selection.')
+    else:
+        movie_rating = percent_pos['Percentage of Positive Reviews'][percent_pos.index == movie_name]
+        st.write(f'##### The movie \'{movie_name}\' has {movie_rating[0]}% positive reviews')
 
     st.write("### Enter Your Review Here:")
     review = st.text_input('')
@@ -39,5 +54,3 @@ def show_predict_page():
 
         else:
             st.subheader(f'Hey {name}, the review you have entered is: Positive')
-
-
