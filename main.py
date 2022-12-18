046fcdc1e2e0756778a4bd051521c009594c4ea9
Yash Taneja <62446644+yash70@users.noreply.github.com>
2022-12-18 15:10:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,4 @@
-from flask import Flask, request
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-import joblib
+import streamlit as st
+from predict_page import show_predict_page
 
-tf = TfidfVectorizer()
-df = pd.read_csv('IMDB Dataset.csv')
-X = df['review']
-new_data = tf.fit_transform(X)
-
-
-def load_model():
-    with open('final_model.pkl', 'rb') as file:
-        model = joblib.load(file)
-    return model
-
-
-model = load_model()
-
-app = Flask(__name__)
-
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    review = request.form.get('review')
-    review = [review]
-    review = tf.transform(review)
-    ans = model.predict(review)
-
-    if ans == 0:
-        return 'It is a Negative Review'
-    else:
-        return 'It is a Positive Review'
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+show_predict_page()
